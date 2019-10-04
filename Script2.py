42b72513cba1879e4bdc136483a1940597d34bab
Maxwell Enger <38050331+MaxEnger@users.noreply.github.com>
2019-10-04 11:55:42 -0400

    Update Script2.py

--- Script2.py
+++ Script2.py
@@ -1,18 +1,17 @@
-# Import library and Personal API Key
 import pyowm
 owm = pyowm.OWM('9067d6794af11f1a696fe420647c0b95')
 
-#get time
+# get time
 import time
 timenow = time.time()
 
 # Gathering Location
 location = str(input("Please enter the location of the area you are interested in "
-                         "(Use the following format, Worcester,US): "))
+                     "(Use the following format, Worcester,US): "))
 print("The location entered: ", location)
 
 
-# Create Function that gathers user input as location and returns meteorological information for specified location
+# Create Function that utilizes user input and returns meteorological information for a specified location
 def weatherGather(location):
     # Gathering observation data from Open Weather Map
     observation = owm.weather_at_place(location)
@@ -25,38 +24,40 @@ def weatherGather(location):
     clouds = w.get_clouds()
     pressure = w.get_pressure()
 
-    # Sunrise time (GMT UNIXtime or ISO 8601)
+    # Sunrise time
     sunrise = w.get_sunrise_time('unix')  # Sunrise time (GMT UNIXtime or ISO 8601)
-    # Sunset time (GMT UNIXtime or ISO 8601)
+    # Sunset time
     sunset = w.get_sunset_time('unix')
     tnow = int(timenow)
     tsunrise = int(sunrise)
     tsunset = int(sunrise)
-    if tnow < tsunrise:
-      t1 = round((float(tsunrise - tnow)/3600),1)
-      print('it is '+ str(t1) +'h before sunrise')
-    elif tnow < tsunset and tnow > tsunrise:      
-      t2 = round((float(tsunset - tnow)/3600),1)
-      print('it is '+ str(t2) +'h before sunset')
+    if tnow > tsunrise:
+        t1 = round((float(tnow - tsunrise) / 3600), 1)
+        print('It is ' + str(t1) + 'h after sunrise')
+    elif tnow < tsunset and tnow > tsunrise:
+        t2 = round((float(tsunset - tnow) / 3600), 1)
+        print('It is ' + str(t2) + 'h before sunset')
     else:
-      t3 = round((float(tnow - tsunset)/3600),1)
-      print('it is '+ str(t3) +'h after sunset')
-    
+        t3 = round((float(tnow - tsunset) / 3600), 1)
+        print('It is ' + str(t3) + 'h after sunset')
 
     weatherData = [wind, humidity, clouds, pressure['press']]
-    # sun = [sunrise, sunset]
+   
 
     # Parsing the data output from OWM to show current temperature
     t = float(temp['temp'])
 
+    # Assigning temperature to a condition category
     if t >= 100:
         print('According to the most recent measurement, the temperature in ' + location + 'is:', t, ', which is hot.')
     elif 70 <= t < 100:
         print('According to the most recent measurement, the temperature in ' + location + 'is:', t, ', which is warm')
     elif 32 <= t < 70:
-        print('According to the most recent measurement, the temperature in ' + location + ' is:', t, ', which is temperate')
+        print(
+        'According to the most recent measurement, the temperature in ' + location + ' is:', t, ', which is temperate')
     else:
-        print('According to the most recent measurement, the temperature in ' + location + 'is:', t, ', which is very cold')
+        print(
+        'According to the most recent measurement, the temperature in ' + location + 'is:', t, ', which is very cold')
 
     if wind in weatherData:
         print('The most recent measurements also indicate that the wind speed(mph) is: ', wind)
